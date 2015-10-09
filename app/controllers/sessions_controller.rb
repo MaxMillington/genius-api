2bf8042deacf759764b6ebb193641cdf4ca4d991
MaxMillington <maxmillington89@gmail.com>
2015-10-09 11:17:11 -0600

    Oauth is working now thanks to building out the find or create from oauth method with api authentication response date

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    binding.pry
     user = User.find_or_create_from_auth(oauth_data)
 
     if user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
-class User
+class User < ActiveRecord::Base
 
   def self.find_or_create_from_auth(data)
-    binding.pry
+
+    user = User.find_or_create_by(
+        provider: data.provider, uid: data.uid)
+
+    user.name = data.info.name
+    user.token = data.credentials.token
+    user.image_url = data.extra.avatar.small.url
+
+    user.save
+
+    user
 
   end
 
